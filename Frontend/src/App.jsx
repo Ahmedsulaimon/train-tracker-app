9137f22f405113298b313893961d1ac1c45937c4
Ahmed Sulaimon <c1032994@hallam.shu.ac.uk>
2024-02-21 10:13:26 +0000

    made the polylines render on the map based on the data retrieved from the API

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,30 +3,16 @@ import socketIOClient from "socket.io-client";
 import {
   MapContainer,
   TileLayer,
+  Polyline,
   Marker,
   Popup,
-  Polyline,
 } from "react-leaflet";
-import { FaBeer } from "react-icons/fa";
-import Timeline from "./Timeline";
 import "./App.css";
+import { isBefore, isAfter, parseISO } from "date-fns";
 
 function App() {
   const [data, setData] = useState(null);
   const [error, setError] = useState(null);
-  const multiPolyline = [
-    [
-      [51.5, -0.15],
-      [51.5, -0.12],
-      [51.52, -0.12],
-    ],
-    [
-      [51.5, -0.05],
-      [51.51, -0.06],
-      [51.52, -0.06],
-    ],
-  ];
-  //const color = { color: "blue" };
 
   useEffect(() => {
     const socket = socketIOClient("http://localhost:3001");
@@ -43,30 +29,73 @@ function App() {
       socket.disconnect();
     };
   }, []);
+
+  const currentDateTime = new Date();
+
   return (
     <>
       <div>
         {error && <p>Error: {error}</p>}
-        <Timeline />
         <h2>Received Data:</h2>
         <pre>{JSON.stringify(data, null, 2)}</pre>
       </div>
       <div className="text-center">
-        <MapContainer
-          center={[51.505, -0.09]}
-          zoom={13}
-          scrollWheelZoom={false}
-        >
+        <MapContainer center={[51.505, -0.09]} zoom={11} scrollWheelZoom={true}>
           <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
-          <Marker position={[51.5, -0.15]}>
-            <Popup>
-              A pretty CSS3 popup. <br /> Easily customizable.
-            </Popup>
-          </Marker>
-          <Polyline positions={multiPolyline} />
+          {data &&
+            data.allTrainSchedule &&
+            data.ids &&
+            data.allTrainSchedule.map((trainMovements, index) => {
+              const trainInfo = data.ids[index];
+              if (!trainInfo) return null; // This check might be redundant, adjust based on your data structure
+
+              const scheduledDeparture = parseISO(trainInfo.scheduledDeparture);
+              const scheduledArrival = parseISO(trainInfo.scheduledArrival);
+
+              let color = "blue"; // Default for currently running trains
+
+              if (trainInfo.cancelled) {
+                color = "red"; // Train is cancelled
+              } else if (isBefore(currentDateTime, scheduledDeparture)) {
+                return null; // Do not render if departure is in the future
+              } else if (isAfter(currentDateTime, scheduledArrival)) {
+                color = "green"; // Train has already run
+              } // No need for an else statement for the running train condition, as it's the default
+
+              const validMovements = trainMovements.filter(
+                (movement) => movement.latLong
+              );
+              const lastMovement = validMovements[validMovements.length - 1];
+
+              return (
+                <React.Fragment key={index}>
+                  <Polyline
+                    pathOptions={{ color }}
+                    positions={validMovements.map((movement) => [
+                      movement.latLong.latitude,
+                      movement.latLong.longitude,
+                    ])}
+                  />
+                  {lastMovement && trainInfo != null && (
+                    <Marker
+                      position={[
+                        lastMovement.latLong.latitude,
+                        lastMovement.latLong.longitude,
+                      ]}
+                    >
+                      <Popup>
+                        {trainInfo.toc_Name.map((toc) => (
+                          <h3>{toc}</h3>
+                        ))}
+                      </Popup>
+                    </Marker>
+                  )}
+                </React.Fragment>
+              );
+            })}
         </MapContainer>
       </div>
     </>
--- Frontend/src/Timeline.jsx
+++ Frontend/src/Timeline.jsx
@@ -1,36 +1,71 @@
-import React from "react";
-import {
-  VerticalTimeline,
-  VerticalTimelineElement,
-} from "react-vertical-timeline-component";
-import "react-vertical-timeline-component/style.min.css";
-import { FaTrainSubway } from "react-icons/fa6";
-function Timeline() {
-  return (
-    <div className="experience">
-      <VerticalTimeline lineColor="#000">
-        <VerticalTimelineElement
-          className="vertical-timeline-element--education"
-          iconStyle={{ background: "#000", color: "#fff" }}
-          icon={<FaTrainSubway />}
-          date="2000-2014"
-        >
-          <h3>domain</h3>
-          <p>journey from different tiploc</p>
-        </VerticalTimelineElement>
+// import React from "react";
+// import {
+//   VerticalTimeline,
+//   VerticalTimelineElement,
+// } from "react-vertical-timeline-component";
+// import "react-vertical-timeline-component/style.min.css";
+// import { FaTrainSubway } from "react-icons/fa6";
+// function Timeline() {
+//   return (
+//     <div className="experience">
+//       <VerticalTimeline lineColor="#000">
+//         <VerticalTimelineElement
+//           className="vertical-timeline-element--education"
+//           iconStyle={{ background: "#000", color: "#fff" }}
+//           icon={<FaTrainSubway />}
+//           date="2000-2014"
+//         >
+//           <h3>domain</h3>
+//           <p>journey from different tiploc</p>
+//         </VerticalTimelineElement>
 
-        <VerticalTimelineElement
-          className="vertical-timeline-element--education"
-          iconStyle={{ background: "#000", color: "#fff" }}
-          icon={<FaTrainSubway />}
-          date="2000-2014"
-        >
-          <h3>domain</h3>
-          <p>journey from different tiploc</p>
-        </VerticalTimelineElement>
-      </VerticalTimeline>
-    </div>
-  );
-}
+//         <VerticalTimelineElement
+//           className="vertical-timeline-element--education"
+//           iconStyle={{ background: "#000", color: "#fff" }}
+//           icon={<FaTrainSubway />}
+//           date="2000-2014"
+//         >
+//           <h3>domain</h3>
+//           <p>journey from different tiploc</p>
+//         </VerticalTimelineElement>
+//       </VerticalTimeline>
+//     </div>
+//   );
+// }
 
-export default Timeline;
+// export default Timeline;
+// <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={true}>
+// <TileLayer
+//   attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+//   url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+// />
+// {data &&
+//   data.allTrainSchedule &&
+//   data.allTrainSchedule.flatMap((trainMovements, index) =>
+//     trainMovements.map((movement, movementIndex) => (
+//       <>
+//         {/* <p> test{movement.latLong.latitude}</p> */}
+//         {movement.latLong && (
+//           <Marker
+//             key={`${index}-${movementIndex}`}
+//             position={[
+//               movement.latLong.latitude,
+//               movement.latLong.longitude,
+//             ]}
+//             // position={[51.505, -0.09]}
+//           >
+//             <Popup>
+//               A pretty CSS3 popup for <br /> {movement.location}
+//             </Popup>
+//             {/* <Polyline
+//           positions={[
+//             movement.latLong.latitude,
+//             movement.latLong.longitude,
+//           ]}
+//         /> */}
+//           </Marker>
+//         )}
+//       </>
+//     ))
+//   )}
+// </MapContainer>
